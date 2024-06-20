85c0e1fafd18c266b7a8dcf05a6c05abb123bd7a
maceann <158064327+maceann@users.noreply.github.com>
2024-06-20 14:29:01 +0200

    Ajout de commentaire

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 print("Elle veut mangé du pain")
 print(" Mais lui veut des pains au chocolat")
+#G?DSKVKskvclvcvckkxwkv,cwxvkxcwvncxwkvkw,vkxcw,kv,wxck,vkcxx,vkcxw,v,cxw
+#dlxlw,vlc,xvcxw,,vq,vw,xc,vwcx,k,vwkc,vwxk,vwkcvwcv,ckv,cwkx
